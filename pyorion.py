1506ec2d666849d7b71d4cae1c1707e26141dce3
Fabio Viola <desmovalvo@gmail.com>
2015-03-24 14:28:04 +0100

    Added documentation to classes and methods

--- pyorion.py
+++ pyorion.py
@@ -10,18 +10,27 @@ from StringIO import StringIO
 # - should we use getters and setters?
 # - check about APPEND on entity creation
 # - add documentation to each method and class
+# - add parsing of the JSON replies
 
 # Attribute class
 class OrionAttribute:
 
+    """This class handles attributes of the entity of the Orion Context Broker"""
+
     # constructor
     def __init__(self, attr_name, attr_type, attr_value):
+
+        """Initialization method for the OrionAttribute class. Its parameters
+        are the attribute name, the type and the value"""
+
         self.attr_name = attr_name
         self.attr_type = attr_type
         self.attr_value = attr_value
 
     # json representation
     def to_json(self):
+
+        """This method returns the JSON representation of the given attribute"""
         
         # creation of the json object
         json_attr = {}
@@ -36,23 +45,41 @@ class OrionAttribute:
 # Entity class
 class OrionEntity:
 
+    """This class handles entities of the Orion Context Broker"""
+
     # constructor
     def __init__(self, entity_id, entity_type, attrs = []):
+
+        """Initialization method for the OrionEntity class. The required parameters
+        are the entity_id, entity_type """
+
         self.entity_id = entity_id
         self.entity_type = entity_type
         self.attrs = attrs
 
-    # attribute addition
-    def add_attribute(self, attribute):
-        self.attrs.append(attribute)
+    # attributes addition
+    def add_attribute(self, attributes):
+
+        """This method receives a list of attributes that must be bound to the
+        entity and then inserted into the Orion Context Broker"""
+        
+        for a in attributes:
+            self.attrs.append(a)
+
 
     # attribute deletion
     def del_attribute(self, attribute):
+        
+        """It receives an attribute to delete from the entity"""
+
         ind = self.attrs.index(attribute)
         del self.attrs[ind]
         
+
     # json representation
     def to_json(self):
+    
+        """This method returns the JSON representation of the entity"""
 
         # create a json object
         json_entity = {}
@@ -72,17 +99,30 @@ class OrionEntity:
 
 
 # KP class
-class KP:
+class OrionKP:
+
+    """This class implements a knowledge processor able to interact
+    with the Orion Context Broker for doing update, query and subscriptions"""
 
     # constructor
     def __init__(self, host, port, token, debug):
+
+        """Initialization method for the OrionKP class. Required parameters
+        are the host of the CB, the port, an authentication token (or None for
+        no authentication) and a boolean parameter to turn on or off the debug
+        functionalities"""
+
         self.host = host
         self.port = port
         self.token = token
         self.debug = debug
 
+
     # create entity
     def create_entity(self, entity):
+
+        """As the name states it creates an entity in the Orion Context Broker.
+        the expected parameter is an object of the OrionEntity class"""
 
         # build the entity url
         entity_url = "%s:%s/ngsi10/contextEntities/%s" % (self.host, self.port, entity.entity_id)
@@ -116,6 +156,7 @@ class KP:
         # TODO: yet to implement
         pass
 
+
     # query
     # TODO: distinguish between the various query types:
     # - query by entity
@@ -125,6 +166,8 @@ class KP:
     # query by entity id
     def query_by_entity_id(self, entity_id):
         
+        """It performs a query on the CB using a given entity_id (string)"""
+
         # build the query url
         query_url = "%s:%s/ngsi10/contextEntities/%s" % (self.host, self.port, entity_id)
 
@@ -152,6 +195,8 @@ class KP:
     
     # query by entity type
     def query_by_entity_type(self, entity_type):
+
+        "It performs a query using a given entity type (string)"
         
         # build the query url
         query_url = "%s:%s/ngsi10/contextEntityTypes/%s" % (self.host, self.port, entity_type)
